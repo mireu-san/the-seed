2cf1532e137852afbc2018e7ff4effec5ba8bfd1
mireu-san <67046863+mireu-san@users.noreply.github.com>
2022-05-17 23:18:49 +0900

    if stuck, recover it from Piture.module.css, .header (remove note mark)

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,16 +5,36 @@ import Navbar from '../pages/Navbar'
 
 import deco from "../pages/Deco.module.css"
 
+
 const About = () => {
   return (
     <article className={styles.article}>
         <picture className={styles.picture}>
           <div className={styles.container}>
-            <img src={story} className={styles.story} />  
+            <img src={story} alt="wallpaper" className={styles.story} />  
               <h1>About</h1>
-              <p className={deco.record}>
-                This is the about page. May present some brief history from Python to current react and typescript frontend enthusiast. This time, I won't include picture but more details.
+                <p className={deco.record}>
+                  {/* This is the about page. May present some brief history from Python to current react and typescript frontend enthusiast. This time, I won't include picture but more details. */}
+                  <h1>Jongwan Kim</h1>
+                  <h3 className={deco.justTitle}>Self-taught Frontend Web Developer Enthusiast</h3>
+                  <p className={deco.justText}>
+                    Focusing on stack with JavaScript base. React / TypeScript is the main stack I use nowadays.
+                  </p>
+                  <p className={deco.justText}>
+                    Face the fear, Build the future. That's what I'm here for.
+                  </p>
+                </p>
+                {/* This area suppose to be located - bottom */}
+              <p className={deco.recordBottom}>
+                <h1>External Links</h1>
+                  <section className={deco.externalLink}>
+                    <a href="https://github.com/mireu-san" className={deco.link}>Github</a>
+                    <a href="https://www.linkedin.com/in/jongwan-kim-89500a194/" className={deco.link}>LinkedIn</a>
+                    <a href="https://silverlibrary.tistory.com/" className={deco.link}>Blog</a>
+                    <a href="mailto:starmireu@gmail.com" className={deco.link}>Email</a>
+                  </section>
               </p>
+              
           </div>
         </picture>
 
